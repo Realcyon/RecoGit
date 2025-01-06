3f3e8b518359d53ebb20e0bca1d86a0a72512263
valentin dupas <valentin.lc.dupas@gmail.com>
2025-01-06 01:29:46 +0100

    formatting

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ app.use("/public", express.static("public"));
 app.use(bodyParser.json());
 //console.log(path); 
 
-function strFromFile(path, options = {createOnNotFoun: false, defaultContent: ""}){ 
-  try{
+function strFromFile(path, options = { createOnNotFoun: false, defaultContent: "" }) {
+  try {
     return fs.readFileSync(path).toString();
-  }catch(err){
-    if(options.createOnNotFoun){
-      fs.writeFileSync(path,options.defaultContent)
+  } catch (err) {
+    if (options.createOnNotFoun) {
+      fs.writeFileSync(path, options.defaultContent)
     }
 
     return options.defaultContent;
@@ -43,13 +43,13 @@ const views = {
 };
 
 const partials = {
-  header : Handlebars.registerPartial("header",
+  header: Handlebars.registerPartial("header",
     strFromFile(pathJoin(__dirname, "views", "partials", "header.html"))
   ),
-  footer : Handlebars.registerPartial("footer",
+  footer: Handlebars.registerPartial("footer",
     strFromFile(pathJoin(__dirname, "views", "partials", "footer.html"))
   ),
-  navigation : Handlebars.compile(
+  navigation: Handlebars.compile(
     strFromFile(pathJoin(__dirname, "views", "partials", "nav.hbs")))
 };
 
@@ -62,15 +62,14 @@ const dataTitleRegister = JSON.parse(
     }
   )
 );
-console.log(dataTitleRegister)
-const sendNavigation = partials.navigation({navSection: Object.keys(dataTitleRegister)});
+const sendNavigation = partials.navigation({ navSection: Object.keys(dataTitleRegister) });
 
-function shortNotes(section){
+function shortNotes(section) {
   const NOTE_CHAR_LIMIT = 100;
   //console.log(section);
   const shortenedSection = section.map(s => {
-    if(s.notes.length >= NOTE_CHAR_LIMIT){
-      s = {...s, notes : s.notes.slice(0,NOTE_CHAR_LIMIT) + "…"};
+    if (s.notes.length >= NOTE_CHAR_LIMIT) {
+      s = { ...s, notes: s.notes.slice(0, NOTE_CHAR_LIMIT) + "…" };
     }
 
     return s;
@@ -80,100 +79,96 @@ function shortNotes(section){
 }
 
 app.get("/", (req, res) => {
- let titleSelector=[];
- //console.log(dataTitleRegister);
-// console.log(typeof(dataTitleRegister));
- let listSection = Object.keys(dataTitleRegister);
- console.log("keys" , Object.keys(dataTitleRegister))
+  let titleSelector = [];
+  //console.log(dataTitleRegister);
+  // console.log(typeof(dataTitleRegister));
+  let listSection = Object.keys(dataTitleRegister);
+  console.log("keys", Object.keys(dataTitleRegister))
 
- listSection.sort((a,b) => dataTitleRegister[b].length - dataTitleRegister[a].length)
- //console.log(listSection);
+  listSection.sort((a, b) => dataTitleRegister[b].length - dataTitleRegister[a].length)
+  //console.log(listSection);
 
- listSection.forEach(oneSection => {
+  listSection.forEach(oneSection => {
     const sectionSize = dataTitleRegister[oneSection].length;
-    
-    let randomSelector = Math.floor(Math.random()*sectionSize);
- // coupe des notes aux mot pour limiter leur taille et qu'elle ne perturbent pas la mise en page
-    
 
-    titleSelector.push({"sectionName":oneSection,
-    "sectionLength":sectionSize,
-    "title":dataTitleRegister[oneSection][randomSelector].title,
-    "date":dataTitleRegister[oneSection][randomSelector].additionDate,
-    "notes":dataTitleRegister[oneSection][randomSelector].notes
- })
+    let randomSelector = Math.floor(Math.random() * sectionSize);
+    // coupe des notes aux mot pour limiter leur taille et qu'elle ne perturbent pas la mise en page
 
- });
 
+    titleSelector.push({
+      "sectionName": oneSection,
+      "sectionLength": sectionSize,
+      "title": dataTitleRegister[oneSection][randomSelector].title,
+      "date": dataTitleRegister[oneSection][randomSelector].additionDate,
+      "notes": dataTitleRegister[oneSection][randomSelector].notes
+    })
+  });
 
   //console.log(titleSelector);
   //console.log(titleSelector);
   const shortenedTitleSelector = shortNotes(titleSelector);
   //console.log(shortenedTitleSelector);
-  res.send(views.index({navigation: sendNavigation , section: shortenedTitleSelector }));
+  res.send(views.index({ navigation: sendNavigation, section: shortenedTitleSelector }));
 });
+
 
 app.get("/section/:sectionName", (req, res) => {
   let shortenedSection = dataTitleRegister[req.params.sectionName];
 
-  console.log(req.query);
-  if(req.query.sortType){
+  // console.log(req.query);
+  if (req.query.sortType) {
     const section = dataTitleRegister[req.params.sectionName];
-    console.log(section);
+    // console.log(section);
 
     let cleanSortType = req.query.sortType;
-    if(req.query.sortType[0]=="!"){
+    if (req.query.sortType[0] == "!") {
       cleanSortType = req.query.sortType.slice(1);
     }
 
-    const sortedSection = section.sort((a,b) => {
-
-    if(cleanSortType == "alpha") {
-      const shortestLength = Math.min(a.title.length, b.title.length)
-      const aTitle = a.title.toLowerCase();
-      const bTitle = b.title.toLowerCase();
-      for(let i = 0; i < shortestLength ; i++){
-          if (aTitle[i] != bTitle[i]){
-            if(req.query.sortType[0]=="!"){
+    const sortedSection = section.sort((a, b) => {
+      if (cleanSortType == "alpha") {
+        const shortestLength = Math.min(a.title.length, b.title.length)
+        const aTitle = a.title.toLowerCase();
+        const bTitle = b.title.toLowerCase();
+        for (let i = 0; i < shortestLength; i++) {
+          if (aTitle[i] != bTitle[i]) {
+            if (req.query.sortType[0] == "!") {
               return (bTitle.charCodeAt(i) - aTitle.charCodeAt(i))
-            }else{
+            } else {
               return (aTitle.charCodeAt(i) - bTitle.charCodeAt(i))
             }
           }
+        }
+        return aTitle.length - bTitle.length
       }
-      return aTitle.length - bTitle.length
-    }
 
-    if(cleanSortType == "date"){
-      if(req.query.sortType[0]=="!"){
-        return (a.additionDate - b.additionDate);
-      }else{
-        return (b.additionDate - a.additionDate);
+      if (cleanSortType == "date") {
+        if (req.query.sortType[0] == "!") {
+          return (a.additionDate - b.additionDate);
+        } else {
+          return (b.additionDate - a.additionDate);
+        }
       }
-    }
 
-    return 0
+      return 0
     })
 
-    
     //console.log(sortedSection);
     shortenedSection = shortNotes(sortedSection);
   }
 
-
-
-  if(req.query.search){
-    shortenedSection.sort((a,b)=>{
-      a-b
+  if (req.query.search) {
+    shortenedSection.sort((a, b) => {
+      a - b
     })
   }
 
-//  if(req.query.sortType[0]=="!"){
-//    shortenedSection = shortenedSection.reverse();
-//  }
-// probablement mieux de le mettre coté client
-//  shortenedSection.forEach((piece) => piece.additionDate = piece.additionDate.getTime()
-//  )
+  //  if(req.query.sortType[0]=="!"){
+  //    shortenedSection = shortenedSection.reverse();
+  //  }
+  // probablement mieux de le mettre coté client
+  //  shortenedSection.forEach((piece) => piece.additionDate = piece.additionDate.getTime()
+  //  )
 
   res.send(
     views.section({
@@ -186,28 +181,26 @@ app.get("/section/:sectionName", (req, res) => {
 
 
 app.get("/add", (req, res) => {
-  res.send(views.add({navigation: sendNavigation}));
+  res.send(views.add({ navigation: sendNavigation }));
 });
 
 
-
-app.post("/api/add",(req, res) => {
-
+app.post("/api/add", (req, res) => {
   const request = req.body;
 
-  if(request.title.trim() == ""){
+  if (request.title.trim() == "") {
     res.status(400).send("le titre est obligatoire");
     return;
   }
 
-  if(request.section.trim() == ""){
+  if (request.section.trim() == "") {
     res.status(400).send("la section est obligatoire");
     return;
   }
 
   const storedSection = dataTitleRegister[req.body.section];
-  if(storedSection
-    && storedSection.map((a)=>a.title).includes(req.body.title)){
+  if (storedSection
+    && storedSection.map((a) => a.title).includes(req.body.title)) {
     res.status(400).send("Ce titre est deja en memoire");
     return;
   }
@@ -215,52 +208,50 @@ app.post("/api/add",(req, res) => {
 
   // plutot que d'avoir 2 strategies d'insertion (une quand la section et existe et une quand elle existe pas)
   // on check juste si elle existe pas, et si c'est la cas on la creer
-  if(!(Object.keys(dataTitleRegister).includes(request.section))){
-   
+  if (!(Object.keys(dataTitleRegister).includes(request.section))) {
+
     dataTitleRegister[request.section] = [];
   }
 
   dataTitleRegister[request.section].push({
-    "title":request.title,
-    "additionDate":Date.now(),  // toujours les stocker sous forme de timestamp UTC+0 comme ca on peut generer toutes les version "lisibles par les humains" a partir de la. Mais on stocke la version "neutre" avec toutes les infos et puis c'est un entien 32bits, pas une string
+    "title": request.title,
+    "additionDate": Date.now(),  // toujours les stocker sous forme de timestamp UTC+0 comme ca on peut generer toutes les version "lisibles par les humains" a partir de la. Mais on stocke la version "neutre" avec toutes les infos et puis c'est un entien 32bits, pas une string
     "notes": request.note
   });
 
-  
+
   fs.writeFileSync(
-    path.join(".","public", "titleRegister.json"),JSON.stringify(dataTitleRegister))
+    path.join(".", "public", "titleRegister.json"), JSON.stringify(dataTitleRegister))
 
   res.sendStatus(200);
 })
 
-app.post("/api/section/display",(req, res) => {
+
+app.post("/api/section/display", (req, res) => {
   const request = req.body;
   //console.log(request.title);
   //console.log(request.section);
-  
 
   const resPiece = dataTitleRegister[request.section].find(el => el.title == request.title);
   //console.log(resPiece);
   res.send(resPiece);
 
-  
+
 })
 
-app.post("/api/section/edit",(req, res) => {
+
+app.post("/api/section/edit", (req, res) => {
   const request = req.body;
 
   const replaceIndex = dataTitleRegister[request.section].findIndex((el) => el.title == request.title)
   //console.log(replaceIndex);
 
   dataTitleRegister[request.section][replaceIndex].notes = request.notes
- 
+
   //console.log(dataTitleRegister[request.section]);
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-
-
-
